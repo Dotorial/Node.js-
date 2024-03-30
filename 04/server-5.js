239a280c8fefe0f267cf6540e63c04b4c72db82c
Choi Jong Won <112853383+litehuman@users.noreply.github.com>
2024-03-30 14:53:36 +0900

    Add files via upload

---
+++ 04/server-5.js
@@ -0,0 +1,28 @@
+// 라우팅 연습하기  (결과 비교 파일 : 04\results\server-5.js)
+const http = require("http");
+
+const server = http.createServer((req,res) => {
+    //req.url : 요청 경로
+    //req.method : 요청 방식
+
+    const {url,method}=req; // =>구조 분해 할당
+ 
+    res.setHeader("Content-type","text/plain");
+
+    if(method==="GET" && url==="/home")
+    {
+        req/write("HOME");
+        res.end();
+    }
+    else if(method==="GET" && url==="/about")
+    {
+        res.end("ABOUT");
+    }
+    else{
+        res.end("NOT FOUND");
+    }
+});
+
+server.listen(3000, ()  => {
+   console.log("서버가 실행 중");
+});
