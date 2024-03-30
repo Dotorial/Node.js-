eda99e773a2491e99e8c645cb7a7f2d4104583d5
Choi Jong Won <112853383+litehuman@users.noreply.github.com>
2024-03-30 14:51:41 +0900

    Add files via upload

---
+++ 02/function-2.js
@@ -0,0 +1,13 @@
+// 함수 표현식으로 선언하고 호출하기  (결과 비교 파일 : 02\results\function-2.js)
+let greeting =function(name){ // 함수 선언하고 호출하기  (결과 비교 파일 : 02\results\function-1.js)
+    console.log(`${name} 님, 안녕하세요?`);
+}
+// 함수 선언
+greeting('홍길동');
+
+// 함수 호출
+
+
+// 함수 선언
+
+// 함수 호출
